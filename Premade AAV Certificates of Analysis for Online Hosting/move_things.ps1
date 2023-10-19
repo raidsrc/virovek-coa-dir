b91f3a397940bcfc1be58044666c8ea4cf9ad46b
Ray Legion <ray.chen@virovek.com>
2023-10-19 10:21:11 -0700

    moved move_things away

--- Premade AAV Certificates of Analysis for Online Hosting/move_things.ps1
+++
@@ -1,4 +0,0 @@
-foreach ($folder in Get-ChildItem -Directory -include *AAV* -Recurse) {
-  $n = -join ($folder, ".")
-  Copy-Item -Path $n -Filter "*.pdf" -Recurse -Destination ./a2 -Container:$false
-}
